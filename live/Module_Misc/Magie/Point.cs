639b826a9515eb1faa91b0252c07e89e682fe3b6
Patrick <pschmidt@pasit.nl>
2026-05-12 14:05:49 +0000

    day2

---
+++ live/Module_Misc/Magie/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magie;
+
+internal struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
