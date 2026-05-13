b79c997c1c35f836e1e89f3b11e5d23ccd6f8ad0
Patrick <pschmidt@pasit.nl>
2026-05-13 11:20:23 +0000

    day3

--- Exercises/Module_extra2/HetCern/Program.cs
+++ Exercises/Module_extra2/HetCern/Program.cs
@@ -7,6 +7,24 @@ class Program
         WillemKlein willem = new WillemKlein();
         SimonVdMeer simon = new SimonVdMeer();
 
-        willem.Bereken();
+        
+        //willem.Bereken(simon.Subtract, 2,3);
+
+
+        MathDel m1 = simon.Add;
+        m1 = m1 + simon.Subtract + simon.Add + simon.Subtract + simon.Add;
+        m1 += simon.Subtract;
+
+        foreach (var meth in m1.GetInvocationList())
+        {
+            Console.WriteLine($"{meth.Method.Name}");
+        }
+
+
+        int result = m1(1, 2);
+
+
+
+        Console.WriteLine(result);
     }
 }
--- Exercises/Module_extra2/HetCern/WillemKlein.cs
+++ Exercises/Module_extra2/HetCern/WillemKlein.cs
@@ -4,12 +4,14 @@ using System.Text;
 
 namespace HetCern;
 
+delegate int MathDel(int x, int y);
+
 internal class WillemKlein
 {
-    public void Bereken()
+    public void Bereken(MathDel calc, int a, int b)
     {
         Console.WriteLine("Willem gaat nu rekenen...");
-        int result = 0;
+        int result = calc(a,b);
 
         Console.WriteLine($"Willem kraait: Het resultaat = {result}");
     }
